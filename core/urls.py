b6661f1e284802ee1c2f6739d65c895782e414de
TimurMA <timur.minyazeff@gmail.com>
2023-07-25 11:08:15 +0500

    добавил url к api

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from apps.auth.views import SignupAPI, LoginAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/login/', LoginAPI.as_view()),
+    path('api/v1/signup/', SignupAPI.as_view()),
 ]
