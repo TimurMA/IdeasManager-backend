78a0316f2aa5de6a3b1a6c2a0251ed202f8f161a
TimurMA <timur.minyazeff@gmail.com>
2023-07-26 11:51:48 +0500

    создал модель Идеи

---
+++ apps/ideas_manager/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Idea(models.Model):
+    class StatusType(models.TextChoices):
+        on_approval = 'on approval'
+        on_adoption = 'on adoption'
+        adopted = 'adopted'
+    
+    name = models.CharField(max_length=127, verbose_name="Название идей")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    time_update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=15, choices=StatusType.choices, default='on approval')
+    rating = models.FloatField(default=0.0,)
+    risk = models.FloatField(default=0.0,)
+
