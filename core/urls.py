aab628f4d0f329ca42cc5f6b227eaf9e58ae9fd5
TimurMA <timur.minyazeff@gmail.com>
2023-07-26 11:57:42 +0500

    Сделал роутер для управления идеями

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from apps.auth.views import SignupAPI, LoginAPI
+from apps.ideas_manager.views import IdeasViewSet
+
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'idea_manager', IdeasViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/login/', LoginAPI.as_view()),
     path('api/v1/signup/', SignupAPI.as_view()),
+    path('api/v1/', include(router.urls))
 ]
