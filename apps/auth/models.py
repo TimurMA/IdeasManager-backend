99780cfe903205ff975ab882e380b192573d8820
andreybyrsa <byrsa2004@bk.ru>
2023-07-26 18:28:34 +0500

    Добавил уникальный идентификатор для пользователя

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     class RoleType(models.TextChoices):
-        expert = 'expert'
-        admin = 'admin'
-        initializer = 'initializer'
-        projectOffice = 'projectOffice'
+        expert = "expert"
+        admin = "admin"
+        initializer = "initializer"
+        projectOffice = "projectOffice"
 
-    role = models.CharField(max_length=13, choices=RoleType.choices, default = 'initializer', verbose_name='Роль')
+    token = models.UUIDField(
+        unique=True,
+        db_index=True,
+        null=True,
+        max_length=100,
+        verbose_name="Токен",
+    )
+    role = models.CharField(
+        max_length=13,
+        choices=RoleType.choices,
+        default="initializer",
+        verbose_name="Роль",
+    )
 
     groups = None
     user_permissions = None
-
-
