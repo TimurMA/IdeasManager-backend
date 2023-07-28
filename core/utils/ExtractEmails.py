c7c143d3cab49651cf68c0a116dedf898f0acede
TimurMA <timur.minyazeff@gmail.com>
2023-07-28 23:50:36 +0500

    добавлена утилита чтения фаилов разных форматов

--- core/utils/ExtractEmails.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def extract_emails_from_content(content):
-    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    emails = re.findall(email_pattern, content)
-
-    return emails
