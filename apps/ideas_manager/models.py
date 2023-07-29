5472964d394b6ebcd2e4d4119d0d54bc3bba0926
TimurMA <timur.minyazeff@gmail.com>
2023-07-29 21:28:02 +0500

    добавил внешний ключ по токену в модели идеи

--- apps/ideas_manager/models.py
+++ apps/ideas_manager/models.py
@@ -5,13 +5,14 @@ class Idea(models.Model):
         on_approval = 'на согласовании'
         on_adoption = 'на утверждении'
         adopted = 'утверждено'
-    
+
     name = models.CharField(max_length=127, verbose_name="Название идей")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     time_update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=15, choices=StatusType.choices, default='на согласовании')
     rating = models.FloatField(default=0.0,)
     risk = models.FloatField(default=0.0,)
+    initator = models.ForeignKey('AuthApi.User', on_delete=models.CASCADE, verbose_name='Инициатор', to_field='token')
 
     def __str__(self) -> str:
         return self.name
