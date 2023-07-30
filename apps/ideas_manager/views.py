86723aac2b4cb1e596923a72316a017db34ae106
TimurMA <timur.minyazeff@gmail.com>
2023-07-30 18:00:52 +0500

    добавлена возможность получить список идей иницатора

--- apps/ideas_manager/views.py
+++ apps/ideas_manager/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Idea
@@ -8,5 +7,13 @@ from .serializers import IdeaSerializer
 class IdeasViewSet(ModelViewSet):
     queryset = Idea.objects.all()
     serializer_class = IdeaSerializer
+
+    def list(self, request, *args, **kwargs):
+        if kwargs.get('token', ''):
+            data = self.get_queryset().filter(initator=kwargs.get('token', ''))
+            serializer_data = self.get_serializer(data, many=True)
+            return Response(serializer_data.data)
+
+        return super(IdeasViewSet, self).list(request, *args, **kwargs)
 
 # Create your views here.
--- core/urls.py
+++ core/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('api/v1/invite/<str:token>/', InvitationRegisterAPI.as_view()),
     path('api/v1/invite/', InvitationRegisterAPI.as_view()),
     path('api/v1/', include(router.urls)),
+    path('api/v1/ideas_manager/get_user_ideas/<str:token>/', IdeasViewSet.as_view({'get': 'list'}))
 ]
